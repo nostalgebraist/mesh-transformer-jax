f9d71e2406b5410ed1c1d9b98f59498da75bb9e3
nostalgebraist <nostalgebraist@gmail.com>
2023-02-02 15:15:22 -0800

    better sampling params

--- device_sample.py
+++ device_sample.py
@@ -96,8 +96,8 @@ if __name__ == "__main__":
             batched_tokens = np.array([padded_tokens] * total_batch)
             length = np.ones(total_batch, dtype=np.uint32) * len(tokens)
 
-            output = network.generate(batched_tokens, length, 512, {"top_p": np.ones(total_batch) * 0.9,
-                                                                    "temp": np.ones(total_batch) * 0.75})
+            output = network.generate(batched_tokens, length, 512, {"top_p": np.ones(total_batch) * 0.95,
+                                                                    "temp": np.ones(total_batch) * 1.0})
 
             for idx, o in enumerate(output[1][0][:, :, 0]):
                 print(f"sample {idx}: {repr(tokenizer.decode(o))}")
